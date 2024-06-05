cb6adc7adcef895345b8710037f50efcc450cc5c
UTTAMBSOLUTIONS221 <francisnguru448@gmail.com>
2024-06-05 18:13:13 +0300

    update

---
+++ Uttambsolutionsapi/Controllers/ProductCategoryController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uttambsolutionsapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductCategoryController : ControllerBase
+    {
+    }
+}
